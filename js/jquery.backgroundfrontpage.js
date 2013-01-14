64fe6fec45d37994a1057d2826168e9c5aff6cf4
Thomas Thune Hansen <tth@bellcom.dk>
2013-01-14 14:48:38 +0100

    added check for background images

--- js/jquery.backgroundfrontpage.js
+++ js/jquery.backgroundfrontpage.js
@@ -10,22 +10,24 @@ jQuery(document).ready(function($) {
       i++;
     });
 
-    $.vegas({
-      src: backgrounds[0].src
-    }); 
+    if(backgrounds[0] !== undefined)
+    {
+      $.vegas({
+        src: backgrounds[0].src
+      }); 
+      $.vegas('slideshow', {
+        backgrounds: backgrounds,
+      })('overlay');
 
-    $.vegas('slideshow', {
-      backgrounds: backgrounds,
-    })('overlay');
-
-    Drupal.viewsSlideshowControls.play = function (options) {
-      $.vegas('pause');
-    }
-    Drupal.viewsSlideshowControls.pause = function (options) {
-      $.vegas('pause');
-    }
-    Drupal.viewsSlideshowPagerFields.goToSlide = function (options) {
-      $.vegas('jump', options['slideNum']);
+      Drupal.viewsSlideshowControls.play = function (options) {
+        $.vegas('pause');
+      }
+      Drupal.viewsSlideshowControls.pause = function (options) {
+        $.vegas('pause');
+      }
+      Drupal.viewsSlideshowPagerFields.goToSlide = function (options) {
+        $.vegas('jump', options['slideNum']);
+      }
     }
     $('<div class="slideshow-previous"><i class="previous-arrow">').insertBefore('#views_slideshow_pager_field_item_bottom_aktuelt-panel_pane_3_0');
     $('<div class="slideshow-next"><i class="next-arrow">').insertAfter('#views_slideshow_pager_field_item_bottom_aktuelt-panel_pane_3_2');
@@ -36,5 +38,6 @@ jQuery(document).ready(function($) {
     $('.slideshow-next').click(function(e){
       $('.views_slideshow_controls_text_next > a').click();
     });
+
   //}
 });
