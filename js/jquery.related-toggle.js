ab9e72f4abcb58659391982fd7ee9e3405afaeac
Thomas Thune Hansen <tth@bellcom.dk>
2013-01-18 09:19:39 +0100

    added pointer to button, updated js

--- js/jquery.related-toggle.js
+++ js/jquery.related-toggle.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($){
   $('.toggle-related').find('.button').click(function(){
     $(this).toggleClass('open');
-    $('.panel-region-stack2').find('.pane-node-field-reference').toggle();
+    $('.panel-region-stack2').find('.pane-node-field-mediafile').toggle();
     $('.panel-region-stack2').find('.pane-selvbetjening').toggle();
     $('.panel-region-stack2').find('.pane-kle-lonks-boks-panel-pane-1').toggle();
   });
