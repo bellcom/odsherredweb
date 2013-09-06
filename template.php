68209c7014dee29a407b22682cec86ce39765265
Stanislav <trail@trail-laptop.(none)>
2013-09-06 12:02:34 +0300

    added check into node submit to handle creation of content page content type

--- template.php
+++ template.php
@@ -70,10 +70,58 @@ function odsherredweb_form_alter(&$form, &$form_state, $form_id) {
         $form['field_billede'][LANGUAGE_NONE][$key]['#element_validate'] = array('odsherredweb_image_alt_text_required_validate');
       }
     }
-
+    $form['#validate'][] = 'odswerredweb_content_node_form_validate';
   }
-
 }
+
+function odswerredweb_content_node_form_validate($form, &$form_state){
+  global $user;
+
+  if(!user_access('administer site configuration') && !in_array('Administrator', $user->roles) && !in_array('Webmaster', $user->roles) && !in_array('Webredaktør', $user->roles)){
+    //drupal_set_message(print_r($form_state['input']['field_termref_sted']['und']['value_field'], TRUE));
+    $sted_input = explode(' ', $form_state['input']['field_termref_sted']['und']['value_field']);
+
+    foreach($sted_input as $sted){
+      $sted = str_replace('"', "", $sted);
+      if (empty($sted))
+	continue;
+      //drupal_set_message(print_r($sted, TRUE));
+      $sted_term = taxonomy_get_term_by_name($sted,'egennavne_stednavne');
+      if (empty($sted_term)){
+	form_set_error('field_termref_sted', $sted . ': ' . t('Du kan kun væge de eksisterende navne'));
+      }
+    }
+
+    //drupal_set_message(print_r($form_state['input']['field_organization']['und']['value_field'], TRUE));
+    $org_input = explode(' ', $form_state['input']['field_organization']['und']['value_field']);
+
+    foreach($org_input as $org){
+      $org = str_replace('"', "", $org);
+      if (empty($org))
+	continue;
+      //drupal_set_message(print_r($org, TRUE));
+      $org_term = taxonomy_get_term_by_name($org,'organisation');
+      if (empty($org_term)){
+	form_set_error('field_organization', $org . ': ' . t('Du kan kun vælge de eksisterende organisationer'));
+      }
+    }
+
+    //drupal_set_message(print_r($form_state['input']['field_politics']['und']['value_field'], TRUE));
+    $politics_input = explode(' ', $form_state['input']['field_politics']['und']['value_field']);
+
+    foreach($politics_input as $politics){
+      $politics = str_replace('"', "", $politics);
+      if (empty($politics))
+	continue;
+      //drupal_set_message(print_r($politics, TRUE));
+      $politics_term = taxonomy_get_term_by_name($politics,'politik');
+      if (empty($politics_term)){
+	form_set_error('field_politics', $politics . ': ' . t('Du kan kun vælge de eksisterende politikker'));
+      }
+    }
+  }
+}
+
 /**
  * Custom quick and dirty form validation
  *
