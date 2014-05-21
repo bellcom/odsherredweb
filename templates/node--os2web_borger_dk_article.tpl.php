70b5bad9ab582b83def1437eb0192c4e2561ccd8
Yani0029 <yani@bellcom.dk>
2014-05-21 20:56:17 +0200

    Fix the foldout/in images and add class to borger.dk content type display

--- templates/node--os2web_borger_dk_article.tpl.php
+++ templates/node--os2web_borger_dk_article.tpl.php
@@ -82,7 +82,7 @@
 
 
 <div class="topborgerdk">
-<div class="node-print"><a href="/print/<?php print $node->nid; ?>" target="_blank" title="Undskriv">Udskriv</a></div>
+<div class="pane-1 del-udskriv float-right"><a href="/print/<?php print $node->nid; ?>" target="_blank" title="Undskriv">Udskriv</a></div>
 <?php print render($region['preface_first']); ?>
 </div>
 
